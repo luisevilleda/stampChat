d7b38c01c7b797a9904a223eb30ae42ead7f53b8
Luis Villeda <luisevilleda@gmail.com>
2017-01-10 00:23:25 -0500

    Implement photo picker functionality

---
+++ app/components/photoPicker/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Platform, View, Text, TouchableOpacity, Image } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+export default class PhotoPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoUrl: null,
+    };
+
+    // this.selectPhotoTapped = this.selectPhotoTapped.bind(this);
+  }
+
+  selectPhotoTapped() {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true,
+      },
+    };
+    console.log(ImagePicker)
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        let source;
+
+        // You can display the image using either:
+        // source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+
+        // Or:
+        if (Platform.OS === 'android') {
+          source = { uri: response.uri, isStatic: true };
+        } else {
+          source = { uri: response.uri.replace('file://', ''), isStatic: true };
+        }
+        this.setState({
+          photoUrl: source,
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+          <View>
+            { this.state.photoUrl === null ? <Text>Select a Photo</Text> :
+            <Image />
+            }
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
