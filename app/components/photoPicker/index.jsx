c53114004bde26044b9ee4e46034227871686a79
Luis Villeda <luisevilleda@gmail.com>
2017-01-09 23:47:42 -0500

    Initilize photo picker component

---
+++ app/components/photoPicker/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { CameraRoll, View, Text } from 'react-native';
+
+export default class PhotoPicker extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
