0bce595e949050f1d8d0e9cf5077568f98c04355
Luis Villeda <luisevilleda@gmail.com>
2017-01-10 22:56:44 -0500

    Install Lob API package

--- app/utilities/apiCalls.js
+++ app/utilities/apiCalls.js
@@ -1,3 +1,6 @@
+import { APIKEY } from './credentials';
+import Lob('APIKEY') from 'lob';
+
 export default {
   postPostcard: (photo, message, sender, recipient) => {
     fetch('https:api.lob.com/v1/postcards', {
---
+++ app/utilities/credentials.example.js
@@ -0,0 +1,3 @@
+export default {
+  APIKEY: 'KEY_GOES_HERE',
+};
